611573872c31558896ffe644131228ef80d50f2a
Christopher Jacoby <christopher.jacoby@gmail.com>
2012-12-10 00:25:15 -0500

    added kmeans, in both offline and online formats.

---
+++ mirlib/feature_analysis/kmeans.py
@@ -0,0 +1,92 @@
+import numpy as np
+from scipy.spatial.distance import euclidean
+
+def euclid_dist(vec1, vec2):
+    if len(vec1.shape) == 2:
+        return np.sqrt(np.sum(np.power(np.subtract(vec1, vec2), 2), axis=1))
+    else:
+        return np.sqrt(np.sum(np.power(np.subtract(vec1, vec2), 2)))
+
+def GetNearestCentroids(data, centroids):
+    # Use Euclidean Distance to get the nearest centroids for each point
+    k = len(centroids)
+    if len(data.shape) == 2:
+        nDim = len(data)
+    else:
+        nDim = 1
+    dist = np.zeros([k, nDim]) # the dist matrix for each centroid
+        
+    for i in range(k):
+        dist[i] = euclid_dist(data, centroids[i])
+
+    return dist.argmin(axis=0)
+
+class kmeans_runner:
+    def __init__(self, k, update_factor=0.01, end_thresh=0.01):
+        ''' k is the number of centroids to use. '''
+        self.k_classes = k
+        self.k_counts = np.zeros(self.k_classes)
+        self.itr_count = 0
+        self.stop_flag = False
+        self.update_factor = update_factor
+        self.end_thresh = end_thresh
+
+    def init_data(self, data):
+        ''' Given an array of data, compute initial centroids, choosing random data
+        to start with.
+        returns: the centroids.'''
+        self.data = data
+        self.data_dim = data.shape[0]
+        self.feature_dim = data.shape[1]
+
+        # Initialize the centroids to a random data point
+        rand_index = np.random.randint(0, self.data_dim, self.k_classes)
+        
+        self.centroids = data[rand_index]
+
+    def iterate(self):
+
+        self.UpdateCentroids()
+        self.itr_count += 1
+
+        if (self.update_changes < self.end_thresh).all():
+            self.stop_flag = True
+
+    def UpdateCentroids_Offline(self):
+        nearest_centroids = GetNearestCentroids(self.data, self.centroids)
+        centroid_update = np.zeros(self.centroids.shape)
+
+        for i in range(self.k_classes):
+            centroid_update[i] = np.mean( self.data[(nearest_centroid == i).nonzero()] )
+
+        self.update_changes = centroid_update - self.centroids
+        self.centroids = self.centroids + self.update_changes * self.update_factor
+
+    def UpdateCentroids_Online(self, data):
+        # because we're sending only one point in here, should return the index
+        # of the nearest centroid
+        nearest_centroid = GetNearestCentroids(data, self.centroids)
+        self.k_counts[nearest_centroid] += 1
+
+        # m_i = m_i + (1/n_i) * (x - m_i)
+        self.centroids[nearest_centroid] += (1 / np.float(self.k_counts[nearest_centroid])) * (data - self.centroids[nearest_centroid])
+
+    def run_offline(self, data):
+        self.init_data(data)
+        while self.stop_flag is False:
+            self.iterate()
+
+        return self.centroids, self.itr_count
+
+    def iterate_online(self, data):
+        self.UpdateCentroids_Online(data)        
+        return self.centroids, self.itr_count
+
+def kmeans(data, k, update, thresh):
+    kmr = kmeans_runner(k, update, thresh)
+    return kmr.run_offline(data)
+
+def vq(data, centroids):
+    return GetNearestCentroids(data, centroids)
+    
+    
--- mirlib_test/kmeans_test.py
+++ mirlib_test/kmeans_test.py
@@ -1,7 +1,129 @@
+import numpy as np
+from numpy import vstack, array
+from numpy.random import rand
 from mirlib.feature_analysis import kmeans
+from pylab import *
+import time
+
+import argparse
+
+def kmr_test_plot(data, k, learn_fact, end_thresh):
+    ion()
+    fig = figure()
+    ax = fig.add_subplot(111)
+    ax.grid(True)
+
+    # get k centroids
+    kmr = kmeans.kmeans_runner(k, learn_fact, end_thresh)
+    kmr.init_data(data)
+    print kmr.centroids
+
+    plot(data[:,0], data[:,1], 'o')
+
+    while kmr.stop_flag is False:
+        kmr.iterate()
+        #print kmr.centroids, kmr.itr_count
+        plot(kmr.centroids[:, 0], kmr.centroids[:, 1], 'sr')
+        time.sleep(.2)
+        draw()
+
+    ioff()
+    show()
+    print kmr.itr_count, kmr.centroids
+
+def test(data, k, learn_fact, end_thresh):
+    centroids, nItr = kmeans.kmeans(data, k, learn_fact, end_thresh)
+    print "N Iterations: %d" % (nItr)
+    print "Means:", centroids
+
+def onlineplot(data, k, learn_fact, end_thresh, on_denom):
+    print "onlineplot", k, learn_fact, end_thresh, on_denom
+
+    ion()
+    fig = figure()
+    ax = fig.add_subplot(111)
+    ax.grid(True)
+    ax.set_ylim((-1.5, 1.5))
+    ax.set_xlim((-1.5, 1.5))
+
+    # get k centroids
+    kmr = kmeans.kmeans_runner(k, learn_fact, end_thresh)
+
+    cutoffIdx = np.ceil(len(data) / np.float(on_denom))
+    np.random.shuffle(data)
+    initialData = data[:cutoffIdx]
+    remainingData = data[cutoffIdx:]
+                        
+    kmr.init_data(initialData)
+
+    print "Initial:", kmr.centroids
+
+    plot(initialData[:,0], initialData[:,1], 'o')
+
+    for point in remainingData:
+        kmr.iterate_online(point)
+        
+        plot(kmr.centroids[:, 0], kmr.centroids[:, 1], 'sr')
+        plot(point[0], point[1], 'ob')
+        #time.sleep(.)
+        draw()
+
+    ioff()
+    show()
+    print "Final Counts:", kmr.k_counts
+    print kmr.centroids
+
+def onlinetest(data, k, learn_fact, end_thresh, on_denom):
+    print "onlinetest", k, learn_fact, end_thresh, on_denom
+    kmr = kmeans.kmeans_runner(k, learn_fact, end_thresh)
+
+    cutoffIdx = np.ceil(len(data) / np.float(on_denom))
+    np.random.shuffle(data)
+    kmr.init_data(data[:(cutoffIdx)])
+
+    print "Initial:", kmr.centroids
+
+    for point in data[cutoffIdx:]:
+        kmr.iterate_online(point)
+
+    print "Final Counts:", kmr.k_counts
+    print kmr.centroids
+    
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--k_classes", help="Number of test classes",
+                        default=4, type=int)
+    parser.add_argument("-u", "--update_mult", help="Learning Multiplier for each step",
+                        default=0.01, type=float)
+    parser.add_argument("-t", "--end_thresh", help="Distance threshold to end",
+                        default=0.01, type=float)
+
+    # Define program modes
+    subparsers = parser.add_subparsers(help="Program Mode Help")
+    parser_plot = subparsers.add_parser("plot", help="Run interactive plot")
+    parser_plot.set_defaults(func=kmr_test_plot)
+    parser_test = subparsers.add_parser("test", help="Run kmeans test")
+    parser_test.set_defaults(func=test)
+    parser_onlineplot = subparsers.add_parser("onlineplot", help="Run online kmeans test")
+    parser_onlineplot.set_defaults(func=onlineplot)
+    parser_onlineplot.add_argument("--dim_denom", help="Denominator of the fraction of the dataset that is to be used with each increment", default=6, type=int)
+    parser_onlinetest = subparsers.add_parser("onlinetest", help="Run online kmeans test")
+    parser_onlinetest.set_defaults(func=onlinetest)
+    parser_onlinetest.add_argument("--dim_denom", help="Denominator of the fraction of the dataset that is to be used with each increment", default=6, type=int)
+    args = parser.parse_args()
+    
     print "mirlib k-means test"
+
+    print "1. Create a test set"
+    # data generation
+    data = vstack((rand(150,2) + array([.5,.5]), rand(150,2), (rand(150,2) - array([.75, 1.2])) ))
+
+    print "2. Run k-means"
+    if args.func is not onlineplot and args.func is not onlinetest:
+        args.func(data, args.k_classes, args.update_mult, args.end_thresh)
+    else:
+        args.func(data, args.k_classes, args.update_mult, args.end_thresh, args.dim_denom)
 
 if __name__ == "__main__":
     main()
