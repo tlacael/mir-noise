c277fbe54be953bf00b268e07b8f68ee92261bd6
Christopher Jacoby <christopher.jacoby@gmail.com>
2012-12-09 19:39:58 -0500

    initial test code

---
+++ mirlib_test/generate_test_audio.py
@@ -0,0 +1,8 @@
+import numpy as np
+import marlib.matlab as M
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
---
+++ mirlib_test/kmeans_test.py
@@ -0,0 +1,7 @@
+from mirlib.feature_analysis import kmeans
+
+def main():
+    print "mirlib k-means test"
+
+if __name__ == "__main__":
+    main()
