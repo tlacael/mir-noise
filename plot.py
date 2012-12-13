e1017974ca40690c803ca275b6bcb64d949a1ff9
Christopher Jacoby <christopher.jacoby@gmail.com>
2012-12-13 18:06:08 -0500

    added k-means centroids to plot

--- plot.py
+++ plot.py
@@ -1,9 +1,11 @@
 from matplotlib.pylab import *
 
-def plot(mfcc):
+def plot(mfcc, centroids):
 
-    fig, (ax1) = subplots(1)
+    fig, (ax1, ax2) = subplots(2)
     fig.subplots_adjust(hspace=.5)
 
     ax1.imshow(mfcc.T, interpolation='nearest', origin='lower', aspect='auto')
+    
+    ax2.imshow(centroids.T, interpolation='nearest', origin='lower', aspect='auto')
     show()
